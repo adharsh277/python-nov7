a190b9dc39c4dad62d27a1fad9bad7b9bd348e2a
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-11-07 23:58:34 +0530

    Add files via upload

---
+++ pythnday123.py
@@ -0,0 +1,49 @@
+name = 'nora '
+likes = 'likes the colour '
+color = "Blue"
+age = ' and she is just 15'
+grades = [67, 100, 87, 56]
+print(name)
+print(name+likes+color+age)
+print(grades[2])
+mylist = [1, 34, 56, 98]
+my_list = ['rajiiev', 'manojk jayan', 'chronic bahelor',
+           'tipu sulathan', 'chandra gupta maurya']
+x = 'chandragupta was a extra ordinary king of  the gupta empire and his period of rulling was secure and happines moments'
+print(x.upper())
+print(x.lower())
+print(my_list)
+print(mylist)
+print(mylist[2])
+print(mylist[-1])
+print(my_list[2])
+print(my_list[1])
+print(my_list[4])
+print(x.replace('gupta', 'maurya'))
+print(x.replace('g', 'm'))
+print(x.upper())
+print(x.lower())
+# ygfs
+
+mylist = [40, 20, 43, 56, 98, 89, 90, 87]
+mylist[1:2] = [58, 300]
+mylist[1:5] = [2000, 300]
+mylist[0:4] = [200, 300]
+mylist[0:6] = [200, 3000]
+mylist[1:3] = [200, 300]
+mylist[-6:2] = [200, 300]
+mylist[1:-8] = [200, 300]
+mylist[1:-6] = [200, 300]
+mylist[-1:6] = [200, 300]
+print(mylist)
+mylist.clear()
+print(mylist)
+# arithmetic operator
+print(100+10)
+print(1056-20)
+print(1090-20)
+print(107*20)
+print(106/20)
+print(105//20)
+print(105 % 20)
+print(109**20)
