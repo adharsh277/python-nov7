01063a7ce1bd656468dd570ed2c9efde69ed3167
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-11-07 23:59:08 +0530

    Add files via upload

---
+++ python66.py
@@ -0,0 +1,44 @@
+def my_function():
+    print("Hello from a function")
+
+
+print("igi")
+
+
+def my_function():
+    print("Hello from a function shubhu suuiii is going to do some thing which is unstoppable so dont do it that for  som of this kinndaa purposee")
+
+
+print(653624626745)
+print(222//5)
+print(89 == 67)
+print(344-553)
+print(768+476)
+print(7626/2)
+print("oihodho")
+print("9898=324")
+
+
+my_function()
+
+# yfuvh
+
+
+def my_function(fname):
+    print(fname + " taraqrueyn")
+
+
+my_function("daemon")
+my_function("Thoris")
+my_function("aegon")
+my_function("auagyfcih")
+my_function("danereyars")
+my_function("rishi sunak")
+# ghjjkl
+
+
+def my_function(fname, lname):
+    print(fname + " " + lname)
+
+
+my_function("Emil", "Refsnes")
